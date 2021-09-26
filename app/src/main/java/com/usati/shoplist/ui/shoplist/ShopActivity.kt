f52174063de028adfc62f788e122388c7eeeb25f
Umesh Sati <umeshsati54@gmail.com>
2021-09-26 16:46:47 +0530

    Without kodein DI.

--- app/src/main/java/com/usati/shoplist/ui/shoplist/ShopActivity.kt
+++ app/src/main/java/com/usati/shoplist/ui/shoplist/ShopActivity.kt
@@ -6,26 +6,29 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.usati.shoplist.R
+import com.usati.shoplist.data.db.ShopDatabase
 import com.usati.shoplist.data.db.entities.ShopItem
+import com.usati.shoplist.data.repositories.ShopRepository
 import com.usati.shoplist.other.ShopItemAdapter
 import kotlinx.android.synthetic.main.activity_shop.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
 
-class ShopActivity : AppCompatActivity(), KodeinAware {
-    override val kodein by kodein()
-    private val factory: ShopViewModelFactory by instance()
+class ShopActivity : AppCompatActivity()//, KodeinAware
+{
+    //override val kodein by kodein()
+    //private val factory: ShopViewModelFactory by instance()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
 
-        /*val database = ShopDatabase(this)
+        val database = ShopDatabase(this)
         val repository = ShopRepository(database)
         val factory = ShopViewModelFactory(repository)
-         */
+
         val viewModel = ViewModelProvider(this,factory).get(ShopViewModel::class.java)
 
         val adapter = ShopItemAdapter(listOf(), viewModel)
